7e0d6cf2c0b20eff2b4e97b711e49d464335afd6
Dmitrii <dmitry.pilipenko@centro.team>
2024-04-05 02:38:00 +0300

    Add documents, company, taxation dialogs

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 import logging
 import os
+from warnings import filterwarnings
 
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler, MessageHandler, filters, Application, ContextTypes, \
-    CallbackQueryHandler, CallbackContext
+    CallbackQueryHandler, CallbackContext, ConversationHandler
 from google.cloud import dialogflow
 from google.api_core.exceptions import InvalidArgument
+from telegram.warnings import PTBUserWarning
 
 from config import CONFIG
 from lang.language import Language
+
+filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
 
 # Enable logging
 logging.basicConfig(
@@ -17,8 +21,12 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
+START_ROUTES, END_ROUTES = range(2)
+
+DOCUMENTS, COMPANY, TAXATION, = range(3)
 
 class TelegramBot:
+
     def __init__(self, telegram_token, dialogflow_credentials_path, dialogflow_project_id, language: Language):
         self.language = language
         self.telegram_token = telegram_token
@@ -52,21 +60,7 @@ class TelegramBot:
             if result['intent'] == 'dialog.end':
                 await update.message.reply_text(self.language.get_text('end_msg'))
 
-            await self.help_command(update, context, 'second_msg')
-
-    async def button_click(self, update: Update, context: CallbackContext) -> None:
-        query = update.callback_query
-        await query.answer()
-
-        if query.data == 'button_documents':
-            await query.edit_message_text(text="click Some documents")
-            # await update.message.reply_text("click documents")
-        elif query.data == 'button_set_up_you_company':
-            await query.edit_message_text(text="click Set up company")
-            # await update.message.reply_text("click Set up company")
-        elif query.data == 'taxation':
-            await query.edit_message_text(text="click Taxation")
-            # await update.message.reply_text("click Taxation")
+            await self.help(update, context, 'second_msg')
 
     def set_language(self, lang):
         if lang == 'ru':
@@ -75,27 +69,185 @@ class TelegramBot:
             self.language.set_lang('en')
 
     # /help
-    async def help_command(self, update, context, text='help'):
+    async def help(self, update, context, text='help'):
         self.set_language(update.message.from_user.language_code)
         keyboard = [
-            [InlineKeyboardButton(language.get_text('button_documents'), callback_data='button_documents')],
-            [InlineKeyboardButton(language.get_text('button_set_up_you_company'), callback_data='button_set_up_you_company')],
-            [InlineKeyboardButton(language.get_text('taxation'), callback_data='taxation')],
+            [InlineKeyboardButton(language.get_text('button_documents'), callback_data="documents")],
+            [InlineKeyboardButton(language.get_text('button_set_up_you_company'), callback_data="company")],
+            [InlineKeyboardButton(language.get_text('button_taxation'), callback_data="taxation")],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.message.reply_text(language.get_text(text), reply_markup=reply_markup)
+        return START_ROUTES
 
     # /start
-    async def start_command(self, update, context):
+    async def start(self, update, context):
         self.set_language(update.message.from_user.language_code)
         await update.message.reply_text(language.get_text('start'))
-        await self.help_command(update, context, 'second_msg')
+        return await self.help(update, context, 'second_msg')
+
+    # documents
+    async def documents(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [
+                InlineKeyboardButton(self.language.get_text("documents_visa_bnt"), callback_data="documents_visa_bnt"),
+                InlineKeyboardButton(self.language.get_text("documents_police_bnt"), callback_data="documents_police_bnt"),
+            ],
+            [
+                InlineKeyboardButton(self.language.get_text("documents_translation_btn"), callback_data="documents_translation_btn"),
+                InlineKeyboardButton(self.language.get_text("company_another_bnt"), callback_data="company_another_bnt"),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        # await context.bot.send_message(chat_id=query.from_user.id, text=self.language.get_text('documents'))
+        await query.edit_message_text(
+            text=language.get_text('documents') + "\n" + language.get_text('documents_2'), reply_markup=reply_markup
+        )
+
+    async def documents_visa_bnt(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [
+                InlineKeyboardButton(self.language.get_text("back"), callback_data="documents"),
+                InlineKeyboardButton(self.language.get_text("thanks"), callback_data="more_question")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text=language.get_text('documents_visa'), reply_markup=reply_markup)
+
+    async def documents_police_bnt(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [
+                InlineKeyboardButton(self.language.get_text("back"), callback_data="documents"),
+                InlineKeyboardButton(self.language.get_text("thanks"), callback_data="more_question")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text=language.get_text('documents_police'), reply_markup=reply_markup)
+
+    async def documents_translation_btn(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [
+                InlineKeyboardButton(self.language.get_text("back"), callback_data="documents"),
+                InlineKeyboardButton(self.language.get_text("thanks"), callback_data="more_question")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text=language.get_text('documents_translation'), reply_markup=reply_markup)
+
+    async def documents_another_btn(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        await query.edit_message_text(text=language.get_text('company_another_text'))
+
+    async def more_question(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [InlineKeyboardButton(language.get_text('button_documents'), callback_data="documents")],
+            [InlineKeyboardButton(language.get_text('button_set_up_you_company'), callback_data="company")],
+            [InlineKeyboardButton(language.get_text('button_taxation'), callback_data="taxation")],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(text=language.get_text('more_question'), reply_markup=reply_markup)
+
+    # company
+    async def company(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [
+                InlineKeyboardButton(self.language.get_text("company_types_btn"), callback_data="company_types_btn"),
+                InlineKeyboardButton(self.language.get_text("company_reg_btn"), callback_data="company_reg_btn"),
+            ],
+            [
+                InlineKeyboardButton(self.language.get_text("company_payments_btn"), callback_data="company_payments_btn"),
+                InlineKeyboardButton(self.language.get_text("company_another_bnt"), callback_data="company_another_bnt"),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(
+            text=language.get_text('company'), reply_markup=reply_markup
+        )
+
+    async def company_btn(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        await query.edit_message_text(text=language.get_text('company_res') + language.get_text(query.data))
+
+    async def company_another_btn(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        await query.edit_message_text(text=language.get_text('company_another_text'))
+
+    # taxation
+    async def taxation(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        keyboard = [
+            [
+                InlineKeyboardButton(self.language.get_text("taxation_tax_btn"), callback_data="taxation_tax_btn"),
+                InlineKeyboardButton(self.language.get_text("taxation_calc_btn"), callback_data="taxation_calc_btn"),
+            ],
+            [
+                InlineKeyboardButton(self.language.get_text("taxation_individual_btn"), callback_data="taxation_individual_btn"),
+                InlineKeyboardButton(self.language.get_text("taxation_another_bnt"), callback_data="taxation_another_bnt"),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(
+            text=language.get_text('taxation'), reply_markup=reply_markup
+        )
+
+    async def taxation_btn(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        await query.edit_message_text(text=language.get_text('taxation_res') + language.get_text(query.data))
+
+    async def taxation_another_btn(self, update, context):
+        query = update.callback_query
+        await query.answer()
+        await query.edit_message_text(text=language.get_text('taxation_another_text'))
 
     def run(self):
         application = Application.builder().token(self.telegram_token).build()
-        application.add_handler(CommandHandler("start", self.start_command))
-        application.add_handler(CommandHandler("help", self.help_command))
-        application.add_handler(CallbackQueryHandler(self.button_click))
+
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler("start", self.start)],
+            states={
+                START_ROUTES:[
+                    CallbackQueryHandler(self.documents, pattern="^documents$"),
+                    CallbackQueryHandler(self.documents_visa_bnt, pattern="^documents_visa_bnt$"),
+                    CallbackQueryHandler(self.documents_police_bnt, pattern="^documents_police_bnt$"),
+                    CallbackQueryHandler(self.documents_translation_btn, pattern="^documents_translation_btn$"),
+                    CallbackQueryHandler(self.company_another_btn, pattern="^documents_another_bnt$"),
+                    CallbackQueryHandler(self.more_question, pattern="^more_question"),
+                    # company
+                    CallbackQueryHandler(self.company, pattern="^company$"),
+                    CallbackQueryHandler(self.company_btn, pattern="^company_types_btn$"),
+                    CallbackQueryHandler(self.company_btn, pattern="^company_reg_btn$"),
+                    CallbackQueryHandler(self.company_btn, pattern="^company_payments_btn$"),
+                    CallbackQueryHandler(self.company_another_btn, pattern="^company_another_bnt$"),
+                    # taxation
+                    CallbackQueryHandler(self.taxation, pattern="^taxation$"),
+                    CallbackQueryHandler(self.taxation_btn, pattern="^taxation_tax_btn$"),
+                    CallbackQueryHandler(self.taxation_btn, pattern="^taxation_calc_btn$"),
+                    CallbackQueryHandler(self.taxation_btn, pattern="^taxation_individual_btn$"),
+                    CallbackQueryHandler(self.taxation_another_btn, pattern="^taxation_another_bnt$"),
+                ],
+            },
+            fallbacks=[CommandHandler("start", self.start)],
+        )
+
+        application.add_handler(conv_handler)
+        application.add_handler(CommandHandler("help", self.help))
         application.add_handler(MessageHandler(filters.TEXT, self.income_msg))
         application.run_polling(allowed_updates=Update.ALL_TYPES)
 
--- lang/en.py
+++ lang/en.py
@@ -1,8 +1,9 @@
 LANGUAGE_EN = {
-    'start': "Hi there! 👋Here you can find a selection of important information for a comfortable start to life in Cyprus!",
+    'start': "Hi there! 👋Here you can find a selection of important information for a comfortable "
+             "start to life in Cyprus!",
     'button_documents': "Documents",
     'button_set_up_you_company': "Set up you company",
-    'taxation': "Taxation",
+    'button_taxation': "Taxation",
     'second_msg': [
         "Write your question or choose one of the sections.",
         "Click on the appropriate section or write a question.",
@@ -13,5 +14,54 @@ LANGUAGE_EN = {
         "I'm here if you want to read more information. 👍🏼"
     ],
     'help': "I can provide you with useful information for life in Cyprus! ☀️\n"
-            "Choose one of the sections to get started or write your own question:"
+            "Choose one of the sections to get started or write your own question:",
+
+    "back": "Back",
+    "thanks": "Thanks",
+    "more_question": ["Do you want to know something else?", "Curious to know anything else?",
+                      "Do you have more questions?"],
+
+    # documents
+    "documents": "For relocation to Cyprus, you need to collect a package of documents. The list of documents depends "
+                 "on the type of residence permit you need. There is a summary and links to useful resources.",
+    "documents_2": ["Click the button and find out more:", "To read the information, click on one of the buttons:",
+                    "Select the button and find out the details:"],
+    "documents_visa_bnt": "Visas for entry",
+    "documents_visa": "Citizens of EU countries and holders of residence permits of these countries don't need a visa "
+                      "to enter Cyprus. Third-country nationals can enter Cyprus with a Schengen visa or with a "
+                      "Cypriot national visa. To find complete information about visas and rules for staying in "
+                      "Cyprus, you can check the website of the Migration Department: "
+                      "http://www.moi.gov.cy/moi/crmd/crmd.nsf/home_en/home_en?openform#",
+    "documents_police_bnt": "Police clearance certificate",
+    "documents_police": "⚖️ Police clearance certificate in Cyprus Having lived in Cyprus for six months or more, "
+                        "you can get a Cypriot certificate of good conduct. The certificate is issued at the main "
+                        "police station in Nicosia on weekdays until 15:00. To receive a certificate, you must fill "
+                        "out and sign an application. Download form 📍 Address: Cyprus Police Headquarters Nicosia "
+                        "1478, Cyprus Phone: +357 (22) 808944   http://www.police.gov.cy ❗You need to have copies "
+                        "and originals of your passport and PinkSlip with you.",
+    "documents_translation_btn": "Translation of documents",
+    "documents_translation": "💬 If you need to translate documents, you can find a certified translator in the "
+                             "registry: https://www.pio.gov.cy/en/register-of-sworn-translators.html",
+    "documents_another_bnt": "Another question",
+    "documents_another_text": "What do you want to know?",
+
+    # company
+    "company": "Here you can find basic information on starting your company in Cyprus.",
+    "company_types_btn": "Main types of companies",
+    "company_reg_btn": "How to register a company in Cyprus",
+    "company_payments_btn": "Obligations and payments (Fees)",
+    "company_another_bnt": "Another question",
+    "company_res": "Your topic: ",
+    "company_another_text": "What do you want to know?",
+
+    # taxation
+    "taxation": "Understanding taxes is very important when you arrive in a new country. "
+                "Here is the starting information for you.",
+    "taxation_tax_btn": "How to become a tax resident in Cyprus",
+    "taxation_calc_btn": "Tax calculator",
+    "taxation_individual_btn": "Individual taxation",
+    "taxation_another_bnt": "Another question",
+    "taxation_res": "Your topic: ",
+    "taxation_another_text": "What do you want to know?",
+
 }
